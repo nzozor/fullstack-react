59dea4f26402a8baabcc9d6294cebdbf3549b86c
Gitea <gitea@fake.local>
2020-01-31 17:23:27 +0000

    Working on the app

---
+++ src/app/index.jsx
@@ -0,0 +1,11 @@
+import { store } from './store/index';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Main } from './components/Main'
+
+// console.log(store.getState());
+
+ReactDOM.render(
+    < Main/>,
+    document.getElementById("app")
+);
